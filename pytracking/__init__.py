1d5a775c0f5fd70d28ecd94c3ae8d51e8d2a9d07
minggii <minjikinn@gmail.com>
2022-08-19 14:49:02 +0900

    refactor: remove redundant import

--- pytracking/__init__.py
+++ pytracking/__init__.py
@@ -5,4 +5,3 @@ import pytracking.libs.fourier as fourier
 import pytracking.libs.dcf as dcf
 import pytracking.libs.optimization as optimization
 from pytracking.run_tracker import run_tracker
-from pytracking.run_webcam import run_webcam
